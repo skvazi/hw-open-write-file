3e89215d7ee6a634c92508d05bad54d38245a6f5
Artem Pavlov <artyom_pavlov@bk.ru>
2021-12-22 00:55:33 +0300

    added second function

--- HW_op_file.py
+++ HW_op_file.py
@@ -1,8 +1,9 @@
+from pprint import pprint
+
 
 
 def read_cookbook(file_path):
     cook_book = {}
-
     with open(file_path, encoding='UTF-8') as file:
         for line in file:
             ingredients_list = []
@@ -22,9 +23,33 @@ def read_cookbook(file_path):
                 ingredients_list.append(ingredients)
             file.readline()
             cook_book[dish_name] = ingredients_list
-
     return cook_book
 
 
 result = read_cookbook('recipes.txt')
-print(result)
+
+pprint(result)
+
+
+def get_shop_list_by_dishes(dishes, persons):
+    menu = read_cookbook('recipes.txt')
+    shopping_list = {}
+    try:
+        for dish in dishes:
+            for item in (menu[dish]):
+                items_list = dict([(item['ingredient_name'], {'measure': item['measure'],
+                                                              'quantity': int(item['quantity'])*persons})])
+                if shopping_list.get(item['ingredient_name']):
+                    extra_item = (int(shopping_list[item['ingredient_name']]['quantity']) +
+                                  int(items_list[item['ingredient_name']]['quantity']))
+                    shopping_list[item['ingredient_name']]['quantity'] = extra_item
+
+                else:
+                    shopping_list.update(items_list)
+        print(f"Для приготовления блюд на {persons} человек  нам необходимо купить:")
+        pprint(shopping_list)
+    except KeyError:
+        print("Вы ошиблись в названии блюда, проверьте ввод")
+
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 10)
